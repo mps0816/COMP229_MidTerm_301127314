1d389d49d63476557d651b49219aa4380dd9a61c
Christina May Pakingan <67124490+ce339t@users.noreply.github.com>
2021-10-31 12:57:17 -0400

    first commit

---
+++ server/config/db.js
@@ -0,0 +1,5 @@
+/**
+ * Add your code here to fix URI connection
+ * 
+ */
+module.exports.URI = "mongodb+srv://ce339t:030489Nina@cluster0.0jexa.mongodb.net/BookList?retryWrites=true&w=majority"
---
+++ server/models/book.js
@@ -0,0 +1,18 @@
+let mongoose = require('mongoose');
+
+let booksModel = mongoose.Schema({
+
+    name: String,
+    author: String,
+    published: String,
+    description: String,
+    price: Number
+
+},
+{
+
+    collection: "books"
+
+});
+
+module.exports = mongoose.model('Book', booksModel);
